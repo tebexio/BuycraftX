1d0b623d11109d393a66cdfa13103c98afcdf4d3
Tux <write@imaginarycode.com>
2016-02-09 20:44:50 -0500

    Listener added

--- bungeecord/src/main/java/net/buycraft/plugin/bungeecord/BuycraftListener.java
+++ bungeecord/src/main/java/net/buycraft/plugin/bungeecord/BuycraftListener.java
@@ -1,9 +1,26 @@
 package net.buycraft.plugin.bungeecord;
 
 import lombok.RequiredArgsConstructor;
+import net.buycraft.plugin.data.QueuedPlayer;
+import net.buycraft.plugin.execution.PlayerLoginExecution;
+import net.md_5.bungee.api.event.PostLoginEvent;
 import net.md_5.bungee.api.plugin.Listener;
+import net.md_5.bungee.event.EventHandler;
 
 @RequiredArgsConstructor
 public class BuycraftListener implements Listener {
     private final BuycraftPlugin plugin;
+
+    @EventHandler
+    public void onPostLogin(PostLoginEvent event) {
+        if (plugin.getApiClient() == null) {
+            return;
+        }
+
+        QueuedPlayer qp = plugin.getDuePlayerFetcher().fetchAndRemoveDuePlayer(event.getPlayer().getName());
+        if (qp != null) {
+            plugin.getLogger().info(String.format("Executing login commands for %s...", event.getPlayer().getName()));
+            plugin.getProxy().getScheduler().runAsync(plugin, new PlayerLoginExecution(qp, plugin.getPlatform()));
+        }
+    }
 }
