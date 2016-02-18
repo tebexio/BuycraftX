42cbf276e7e3d62c0aaeae0c72458ba1770456a4
20zinnm <meyerzinn@gmail.com>
2016-02-17 22:19:31 -0600

    Still abstracting.

---
+++ sponge/src/main/java/net/buycraft/plugin/sponge/gui/GuiUtils.java
@@ -0,0 +1,16 @@
+package net.buycraft.plugin.sponge.gui;
+
+/**
+ * Created by meyerzinn on 2/17/16.
+ */
+public class GuiUtils {
+
+    private static GuiUtils ourInstance = new GuiUtils();
+
+    public static GuiUtils getInstance() {
+        return ourInstance;
+    }
+
+    private GuiUtils() {
+    }
+}
---
+++ sponge/src/main/java/net/buycraft/plugin/sponge/gui/PackageIcon.java
@@ -0,0 +1,8 @@
+package net.buycraft.plugin.sponge.gui;
+
+/**
+ * Created by meyerzinn on 2/17/16.
+ */
+public class PackageIcon {
+
+}
