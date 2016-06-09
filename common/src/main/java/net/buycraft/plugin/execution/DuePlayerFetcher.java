5732b4b07face616f2ca5cd5276335961efbc372
Tux <write@imaginarycode.com>
2016-06-08 21:34:20 -0400

    Limit commands to execute for online players to 15 players maximum.
    BuycraftX tries to execute all commands for all online players at once. With a lot of online players, BuycraftX will issue too many requests and the service will enfore a rate-limit.
    
    Only having 15 player's commands be executed every 5 minutes slowly is preferable to having a giant unknown flush of commands execute and not being able to execute more.

--- common/src/main/java/net/buycraft/plugin/execution/DuePlayerFetcher.java
+++ common/src/main/java/net/buycraft/plugin/execution/DuePlayerFetcher.java
@@ -27,6 +27,7 @@ public class DuePlayerFetcher implements Runnable {
 
     private static final int MAXIMUM_PER_PAGE = 250;
     private static final int FALLBACK_CHECK_BACK_SECS = 300;
+    private static final int MAXIMUM_ONLINE_PLAYERS_TO_EXECUTE = 15;
 
     public boolean inProgress() {
         return inProgress.get();
@@ -116,8 +117,10 @@ public class DuePlayerFetcher implements Runnable {
         for (Iterator<QueuedPlayer> it = due.values().iterator(); it.hasNext(); ) {
             QueuedPlayer qp = it.next();
             if (platform.isPlayerOnline(qp)) {
-                processNow.add(qp);
-                it.remove();
+                if (processNow.size() < MAXIMUM_ONLINE_PLAYERS_TO_EXECUTE) {
+                    processNow.add(qp);
+                    it.remove();
+                }
             }
         }
 
@@ -125,9 +128,7 @@ public class DuePlayerFetcher implements Runnable {
             platform.log(Level.INFO, String.format("Executing commands for %d online players...", processNow.size()));
             for (int i = 0; i < processNow.size(); i++) {
                 QueuedPlayer qp = processNow.get(i);
-                // 500ms delay between each player to spread server load for many online players, up to a
-                // maximum of 5 seconds
-                platform.executeAsyncLater(new PlayerCommandExecutor(qp, platform), Math.min(5000, i * 500), TimeUnit.MILLISECONDS);
+                platform.executeAsyncLater(new PlayerCommandExecutor(qp, platform), i + 1, TimeUnit.SECONDS);
             }
         }
     }
