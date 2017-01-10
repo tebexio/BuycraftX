fbf55e42b40edea7b7da70b69cd50fc00437de9a
Tux <write@imaginarycode.com>
2017-01-10 16:52:21 -0500

    Fix JDK version in script

--- travis-build.sh
+++ travis-build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "${TRAVIS_JDK_VERSION}" = "openjdk7" ]; then
+if [ "${TRAVIS_JDK_VERSION}" = "oraclejdk7" ]; then
     mvn clean package -pl bukkit,bungeecord,common,plugin-shared
 else
     mvn clean package
